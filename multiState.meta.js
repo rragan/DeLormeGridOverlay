2422a3ca4802e8da3ef9f643330522d6661dd9af
Richard Ragan <rragan@gmail.com>
2021-09-03 16:43:12 -0700

    Create multiState.meta.js

---
+++ multiState.meta.js
@@ -0,0 +1,37 @@
+// ==UserScript==
+/* globals jQuery, $, L, waitForKeyElements, cloneInto */
+// @name            Delorme Grid Generic State Overlay
+// @author          rragan (derived from cachetur Assistant code)
+// @version       1.0.0.0
+// @description     Companion script for geocaching.com
+// @include         https://www.geocaching.com/play/map*
+// @include         http://www.geocaching.com/play/map*
+// @include         https://www.geocaching.com/map/*
+// @include         http://www.geocaching.com/map/*
+// @include         https://www.geocaching.com/play/map*
+// @include         http://www.geocaching.com/play/map*
+// @include         https://www.geocaching.com/geocache/*
+// @include         http://www.geocaching.com/geocache/*
+// @include         https://www.geocaching.com/seek/cache_details.aspx*
+// @include         https://www.geocaching.com/plan/lists/BM*
+// @include         http://www.geocaching.com/play/geotours/*
+// @include         https://www.geocaching.com/play/geotours/*
+// @include         http://project-gc.com/*
+// @include         https://project-gc.com/*
+// @connect         self
+// @connect         rragan.atwebpages.com
+// @connect         raw.githubusercontent.com
+// @grant           GM_xmlhttpRequest
+// @grant           GM_info
+// @grant           GM_setValue
+// @grant           GM_getValue
+// @grant           GM_openInTab
+// @grant           GM_addStyle
+// @grant           GM_addStyle
+// @grant           unsafeWindow
+// @run-at          document-end
+// @copyright       2021+, rragan
+// @require         https://code.jquery.com/jquery-latest.js
+// @require         https://gist.github.com/raw/2625891/waitForKeyElements.js
+// @downloadURL     https://github.com/rragan/DeLormeGridOverlay/raw/main/multiState.user.js
+// ==/UserScript==
